a15404db2d7ad846c35a44c2512182f24419989f
hacker2023beginer <tsurkovlados@gmail.com>
2026-03-25 02:17:11 +0300

    Update API client

--- src/main/java/com/study/paymentservice/client/RandomNumberClient.java
+++ src/main/java/com/study/paymentservice/client/RandomNumberClient.java
@@ -7,8 +7,14 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RandomNumberClient {
+
     private static final String API_URL = "https://www.randomnumberapi.com/api/v1.0/random?min=1&max=100&count=1";
-    private final RestTemplate restTemplate = new RestTemplate();
+
+    private final RestTemplate restTemplate;
+
+    public RandomNumberClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public int getRandomNumber() {
         int[] response = new int[1];
